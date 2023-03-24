f363d78d67ca1e64842a950632b0b04442f1b438
hanichoye <hana36593@gmail.com>
2023-03-24 02:12:30 -0700

    Task 10

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,15 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
- * @n: integer
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- *
  */
 typedef struct listint_s
 {
