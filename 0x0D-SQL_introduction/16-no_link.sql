0f013b8976f2a72ef9587b54ed15b1d7037470b4
hanichoye <hana36593@gmail.com>
2023-05-18 10:23:33 -0700

    Tasks Completed

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score in the table second_table
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
