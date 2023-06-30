c7d0a6051f2d80c34465675e299137f1afd9259e
hanichoye <hana36593@gmail.com>
2023-06-30 05:37:48 -0700

    Python network

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# The script that displays the body of the response
-
+# The script that displays the body of the response and sends a DELETE request to the URL passed as the first argument
 curl -s "$1" -X DELETE
